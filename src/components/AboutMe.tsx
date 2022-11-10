e98d3ed7351a81774ec4923267bb71b466158f33
Midokhalil1 <midokhalil22@gmail.com>
2022-11-10 17:18:41 -0500

    ts.done

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,22 @@
+export interface Person {
+    name: string,
+    age: number,
+    awesome: boolean
+}
+
+interface AboutMeProps {
+    person: Person
+
+}
+
+export default function AboutMe({ person }: AboutMeProps) {
+    return (
+        <>
+            <h2> Name: {person.name} </h2>
+            <h3>Age: {person.age}</h3>
+            <h3> I am {(person.awesome) ? 'Awesome' : 'meh'}.</h3>
+
+
+        </>
+    )
+}
